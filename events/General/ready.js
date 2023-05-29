4810966e9a4cab6002ec8e38d2ffbb24fc5c51a9
izunya <whdtj5615@naver.com>
2023-05-29 11:30:57 +0000

    p!

--- events/General/ready.js
+++ events/General/ready.js
@@ -8,5 +8,5 @@ client.once(Discord.Events.ClientReady, async (client) => {
     const owner = client.users.cache.get(client.config.ownerID)
     console.log(`Logged in as ${client.user.tag} | ${client.user.id}!`);
     if(owner.tag) console.log(`안녕하세요! ${owner.username}님!`);
-    client.user.setPresence({ activities: [{ name: `@${client.user.username} help | /help`, type: Discord.ActivityType.Playing }]});
+    client.user.setPresence({ activities: [{ name: `p!help | /help`, type: Discord.ActivityType.Playing }]});
 })
